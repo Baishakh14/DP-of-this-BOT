53f6c2458618fe6d93b476d4964d35d89a45629d
Mahfiz Rahman Baishakh <146338298+Baishakh14@users.noreply.github.com>
2026-07-05 14:16:20 +0600

    Minimizing Coins Iterative.cpp

---
+++ Minimizing Coins Iterative.cpp
@@ -0,0 +1,57 @@
+/*
+Allaih is Almighty
+Bismillahhi Rahmanir Rahim
+Baishakh
+*/
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+const ll mod = 1e9+7;
+// s.order_of_key(x) -> values total are small than x
+// *s.find_by_order(x)  -> kth small elemnt (index) in pbds
+const ll mx = 1e6 + 10;
+ll n,k;
+vector<ll>ab;
+vector<ll>dp(mx,-1);
+ll find(ll num)
+{
+    if(num == 0) return 0;
+    if(dp[num] != - 1) return dp[num];
+    ll lage = 1e9;
+    for(ll i = 0;i<n;i++)
+    {
+        if(num >= ab[i])
+        {
+           lage = min(lage,1 + find(num - ab[i]));
+        }
+    }
+    return dp[num] = lage;
+}
+int main()
+{
+    cin>>n>>k;
+    ab.resize(n);
+    for(auto &it : ab) cin>>it;
+    vector<ll>hobe(k + 1,INT_MAX);
+    hobe[0] = 0;
+    for(int coin = 1;coin<=k;coin++)
+    {
+        for(int i = 0;i<n;i++)
+        {
+            if(coin >= ab[i])
+            {
+                hobe[coin] = min(hobe[coin],1 + hobe[coin - ab[i]]);
+            }
+        }
+    }
+    if(hobe[k] >= INT_MAX) cout<<-1<<endl;
+    else cout<<hobe[k]<<endl;
+  return 0;
+}
+
+
+//Tata Goodbye Khatam
